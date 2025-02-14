ca1589cc817ae5d2004d4a66bfdda660da5941d6
vistor <vistormu@gmail.com>
2025-02-14 15:47:55 +0100

    v0.0.14

--- utils/num/mean.go
+++ utils/num/mean.go
@@ -16,6 +16,32 @@ func Mean[T Number](values []T) T {
     return sum / T(length)
 }
 
+func MultiMean[T Number](values [][]T) []T {
+	if len(values) == 0 {
+		return nil
+	}
+
+	// Get the number of indices in each inner slice.
+	numIndices := len(values[0])
+	means := make([]T, numIndices)
+
+	// Iterate over each index position.
+	for i := 0; i < numIndices; i++ {
+		var sum T
+		// Sum the i-th element from each inner slice.
+		for _, row := range values {
+			if len(row) != numIndices {
+				panic("all inner slices must have the same length")
+			}
+			sum += row[i]
+		}
+		// Compute the mean for index i.
+		means[i] = sum / T(len(values))
+	}
+
+	return means
+}
+
 func StdDev[T Number](values []T) T {
 	if len(values) == 0 {
 		return 0
