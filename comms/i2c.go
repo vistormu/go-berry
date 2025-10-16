19c772aa8ed30430c3b035fb074892dc3b535bc6
vistor <vistormu@gmail.com>
2025-10-16 10:32:20 +0200

    v0.0.18

--- comms/i2c.go
+++ comms/i2c.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 	"os"
 
-    "github.com/vistormu/go-berry/errors"
+	"github.com/vistormu/go-berry/errors"
 )
 
- // #include <linux/i2c-dev.h>
+// #include <linux/i2c-dev.h>
 import "C"
 
 type I2C struct {
@@ -22,50 +22,50 @@ func NewI2C(addr uint8, bus int) (*I2C, error) {
 		return nil, errors.New(errors.I2C_OPEN, err.Error())
 	}
 
-    err = ioctl(f.Fd(), C.I2C_SLAVE, uintptr(addr))
-    if err != nil {
-        return nil, errors.New(errors.I2C_OPEN, err.Error())
-    }
+	err = ioctl(f.Fd(), C.I2C_SLAVE, uintptr(addr))
+	if err != nil {
+		return nil, errors.New(errors.I2C_OPEN, err.Error())
+	}
 
-    return &I2C{
-        rc: f, 
-        bus: bus, 
-        addr: addr,
-    }, nil
+	return &I2C{
+		rc:   f,
+		bus:  bus,
+		addr: addr,
+	}, nil
 }
 
 func (i *I2C) Read(registers []byte, nBytes []int) ([]byte, error) {
-    if len(registers) > 0 && isConsecutive(registers) {
-        startRegister := registers[0]
-        totalBytes := sum(nBytes)
+	if len(registers) > 0 && isConsecutive(registers) {
+		startRegister := registers[0]
+		totalBytes := sum(nBytes)
 
-        return i.readBulk(startRegister, totalBytes)
-    }
+		return i.readBulk(startRegister, totalBytes)
+	}
 
-    result := make([]byte, sum(nBytes))
-    offset := 0
+	result := make([]byte, sum(nBytes))
+	offset := 0
 
-    for j, reg := range registers {
-        n := nBytes[j]
+	for j, reg := range registers {
+		n := nBytes[j]
 
-        if _, err := i.rc.Write([]byte{reg}); err != nil {
-            return nil, errors.New(errors.I2C_READ, reg, err.Error())
-        }
+		if _, err := i.rc.Write([]byte{reg}); err != nil {
+			return nil, errors.New(errors.I2C_READ, reg, err.Error())
+		}
 
-        buf := make([]byte, n)
-        if _, err := i.rc.Read(buf); err != nil {
-            return nil, errors.New(errors.I2C_READ, reg, err.Error())
-        }
+		buf := make([]byte, n)
+		if _, err := i.rc.Read(buf); err != nil {
+			return nil, errors.New(errors.I2C_READ, reg, err.Error())
+		}
 
-        copy(result[offset:], buf)
-        offset += n
-    }
+		copy(result[offset:], buf)
+		offset += n
+	}
 
-    return result, nil
+	return result, nil
 }
 
-func (i *I2C) Write(reg byte, value byte) error {
-	buf := []byte{reg, value}
+func (i *I2C) Write(reg byte, data ...byte) error {
+	buf := append([]byte{reg}, data...)
 	_, err := i.rc.Write(buf)
 	if err != nil {
 		return errors.New(errors.I2C_WRITE, reg, err.Error())
@@ -75,41 +75,41 @@ func (i *I2C) Write(reg byte, value byte) error {
 }
 
 func (i *I2C) Close() error {
-    err := i.rc.Close()
-    if err != nil {
-        return errors.New(errors.I2C_CLOSE, err.Error())
-    }
+	err := i.rc.Close()
+	if err != nil {
+		return errors.New(errors.I2C_CLOSE, err.Error())
+	}
 
 	return nil
 }
 
 func (i *I2C) readBulk(startRegister byte, nBytes int) ([]byte, error) {
-    if _, err := i.rc.Write([]byte{startRegister}); err != nil {
-        return nil, errors.New(errors.I2C_READ, startRegister, err.Error())
-    }
+	if _, err := i.rc.Write([]byte{startRegister}); err != nil {
+		return nil, errors.New(errors.I2C_READ, startRegister, err.Error())
+	}
 
-    result := make([]byte, nBytes)
-    if _, err := i.rc.Read(result); err != nil {
-        return nil, errors.New(errors.I2C_READ, startRegister, err.Error())
-    }
+	result := make([]byte, nBytes)
+	if _, err := i.rc.Read(result); err != nil {
+		return nil, errors.New(errors.I2C_READ, startRegister, err.Error())
+	}
 
-    return result, nil
+	return result, nil
 }
 
 func isConsecutive(registers []byte) bool {
-    for i := 1; i < len(registers); i++ {
-        if registers[i] != registers[i-1]+1 {
-            return false
-        }
-    }
-    return true
+	for i := 1; i < len(registers); i++ {
+		if registers[i] != registers[i-1]+1 {
+			return false
+		}
+	}
+	return true
 }
 
 func sum(values []int) int {
-    result := 0
-    for _, value := range values {
-        result += value
-    }
+	result := 0
+	for _, value := range values {
+		result += value
+	}
 
-    return result
+	return result
 }
